688ee90f0bf3b7deb614849b550b133228bc4964
Ron H <ronald.hirschauer@gmail.com>
2020-07-08 20:21:26 -0400

    Update to App.js and index.css.

--- src/App.js
+++ src/App.js
@@ -16,24 +16,25 @@ import {TodoList} from './todolist-classes';
 // import {PracticeData} from './userdata';
 import PeopleData from './userdata2.js';
 
+
 export class Application extends React.Component{
   render(){
     return (
       <div className="main-div">
         <header className="jumbotron text-center p-5">
-          <h1><img src="/React.png" alt="" className="logo"></img>React Practice Exercises</h1>
+          <h1><img src="/React.png" alt="" className="logo"></img>React Examples</h1>
         </header>
     <Container>
       <Row>
         <h3>1. Simple  Counter:</h3>
-        <p>The first excercise is a simple counter.  There are many examples and tutorials available including this one from www.freecodecamp.org.  They provide excellent excercises for learning React.  This counter is based on their tutorial.  This is the first one I created after reviewing the tutorial.
+        <p>The first excercise is a simple counter.  There are many examples and tutorials available including this one from www.freecodecamp.org.  They provide excellent excercises for learning React.  This counter is based on their tutorial.
         </p>
         <Counter/>
         <hr></hr>
         <Container>
           <Row>
             <h3>2. Render a Simple List:</h3>
-            <p>After watching many React tutorials and completing the freecodecamp tutorials (an excellent educational resource I might add: <a href="https://www.freecodecamp.org/learn/">https://www.freecodecamp.org/learn/</a>), I challenged myself to start creating components in React.  Folowing the counter, this simple list was my next effort.
+            <p>After watching many React tutorials and completing the freecodecamp tutorials (an excellent educational resource I might add: <a href="https://www.freecodecamp.org/learn/">https://www.freecodecamp.org/learn/</a>), I challenged myself to start creating components in React.  Following the counter, this simple list was my next effort.
             </p>
             <ul><NameList/></ul>
           </Row>
@@ -72,9 +73,13 @@ export class Application extends React.Component{
         {/* -------------------------------------- */}
         <Container>
           <h3>7. Rendering A Carousel</h3>
-          <p>This was a interesting process. I tried several approaches.  As a new developer, my ability to modify each approach was limited. The degree of dificulty for each approach varied significantly. I started out trying to create a bootstrap carousel using code from their website. I was having trouble getting it to perform the way I wanted it to.  
-          
-          I started searching for tutorials and examples and I learned a LOT!  There are many ways to create a carousel (sometimes referred to as a slider). My second attempt was using an npm package. This one claimed to be responsive but there were issues with the thumbs at the bottom when resizing to a mobile-size screen (thumbs extended beyond the page). On to the next one! This one performed the best,  and was pretty easy to use.  The npm package can be found here: <a href="https://www.npmjs.com/package/pure-react-carousel">(npm pure-react-carousel).</a>  This is the one I used for the example.  It literally took minutes to put together, and some additional time to modify for this example.  All-in-all, this is a simple and easy to use carousel. Thanks to the folks who created it!
+          <p>There are a variety of methods for creating carousels (also known as sliders). The Bootstrap framework offers a carousel. There are numerous npm packages available as well. They range from very simple to complicated; the functionality and degree of responsiveness coincides with code complexity. Reading the documentation assosciated with the package will provide some information that is helpful when chosing the right package for you project.</p>
+
+          <p>Not all packages are created equal. The initial package used for this example had functionality that including a continuous loop which could be stopped at any point. However, despite its claim to be responsive, there were problems with it rendering properly on mobile devices (tumbs extended beyond the page).</p>
+
+          <p>The next package, and the one used for this example, had less functionality but there were no issues with the functionality. One could optionally add functionality if needed.</p>
+
+          <p>The npm package can be found here: <a href="https://www.npmjs.com/package/pure-react-carousel">(npm pure-react-carousel).</a>  It literally took minutes to put together, including adjustments to the CSS. Thanks to the folks who created it!
           </p>
           <Row className="carouselParent">
             <Carousel/>
@@ -83,7 +88,7 @@ export class Application extends React.Component{
         {/* -------------------------------------- */}
         <Container>
           <h3>8. React Forms/Todo Lists</h3>
-          <p>This is an excercise in the creation of  forms using older and newer coding methods.  There form below are identical in appearance.  However, the example on the left using hooks while the example on the right uses classes and state <a href="https://reactjs.org/docs/hooks-intro.html">React Hooks.</a>
+          <p>This is an excercise in the creation of  forms using older and newer coding methods.  The forms below are identical in appearance.  However, the example on the left using hooks while the example on the right uses classes and state <a href="https://reactjs.org/docs/hooks-intro.html">React Hooks.</a>
           </p>
           <Row>
             <TodoApp/>
@@ -112,17 +117,22 @@ export class Application extends React.Component{
         </Container>  
         <Container>
           <Row>
-            
             <h4>Lifecycle Methods:</h4>
-            <p>The code resulted in a "memory leak" error. This HackMD details the resolution of this error.<a href="https://hackmd.io/K-R5QPlRRnKR7UB5H3TC6g.com"> HackMD</a> Both version (lifecycle and hooks, were styled using the same standard CSS file by using the same classNames, though the contact is slightly different.</p>
-            <p>Memory leaks...... </p>
+            <p>
+              This section is a discussion of lifecylce methods used, including the discovery of multiple ways of writing the code that was discovered when the "memory leaks" error occured, and the resulting investigation of different code to accomplish the desired goal, and resolve the error.  The "in-depth" investigation of coding alternatives and various methods of error resolution to be discussed at more length in the following HackMD article:<a href="https://hackmd.io/zrKlbybdQ66lz1hRW1qsTA"> Investigation I: Lifecycles, state, hooks and memory leaks.</a> 
+            </p>
 
-            <h4>Hooks / Fetch / Async / Await</h4>
-          
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           <h4>Hooks Methods:</h4>
+          <p>This section is a discussion of the function component/hooks used to acheive the goal of fetching data and duplicate the results of the lifecycle methods.  See same HackMD article referenced above.</p>
+          <p><strong>Note:</strong> The discussion of the methods used in this section should be high-level, and reference <strong>Investigation I</strong> where the error is explored and discussed in detail.</p>
           
           
+          </Row>
+        </Container>
+        <Container>
+          <Row>
+
+
           </Row>
         </Container>
       </Row>
