56feb32cc3587572252ce518a62d895d1e069a94
Ron H <ronald.hirschauer@gmail.com>
2020-08-12 18:44:06 -0400

    Directory name: deleting old name files.

--- investigations/promises/src/StringCodeEx/StringChildCode.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-function StringChildCode(props) {
-    return <h4>Hi {props.name}</h4>
-}
-
-export default StringChildCode
-
-// Types of props that can be passed: numbers, booleans, strings...
